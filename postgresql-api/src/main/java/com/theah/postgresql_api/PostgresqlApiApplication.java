8fccce26595d5d7149431293d6b114c035fae9f1
mikek <mikek8025@gmail.com>
2024-12-31 10:51:18 -0600

    Spring Initializer download

---
+++ postgresql-api/src/main/java/com/theah/postgresql_api/PostgresqlApiApplication.java
@@ -0,0 +1,13 @@
+package com.theah.postgresql_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlApiApplication.class, args);
+	}
+
+}
---
+++ postgresql-api/src/test/java/com/theah/postgresql_api/PostgresqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theah.postgresql_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
