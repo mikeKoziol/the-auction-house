d9fdb713429a027ebb52d117e817b447fc6fcbb4
mikek <mikek8025@gmail.com>
2025-01-03 01:40:55 -0600

    Created user + organization models

--- postgresql-api/src/main/java/com/theah/postgresql_api/model/Organization.java
+++ postgresql-api/src/main/java/com/theah/postgresql_api/model/Organization.java
@@ -1,5 +1,78 @@
 package com.theah.postgresql_api.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "organizations")
 public class Organization {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = true)
+    private String description;
+
+    @OneToOne
+    @JoinColumn(nullable = false, insertable = true, updatable = true)
+    private User creatorId;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dateCreated;
+
+    @Column(nullable = false)
+    private Boolean isActive;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(User creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }  
+
 }
--- postgresql-api/src/main/java/com/theah/postgresql_api/model/User.java
+++ postgresql-api/src/main/java/com/theah/postgresql_api/model/User.java
@@ -1,6 +1,7 @@
 package com.theah.postgresql_api.model;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -11,12 +12,98 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
     private String name;
 
     @Column(nullable = false, unique = true)
     private String email;
 
-    
+    @OneToOne
+    @JoinColumn(nullable = true, insertable = true, updatable = true)
+    private Organization organizationId;
 
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dateCreated;
+
+    @Column(nullable = false)
+    private Boolean isActive;
+
+    public enum Role {
+        admin,
+        independent,
+        asset_manager,
+        auctioneer,
+        buyer,
+        viewer
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Organization getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(Organization organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
 
 }
